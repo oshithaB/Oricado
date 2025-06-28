fdf9c1ec360c9a63da48daf28181555b8f18d36d
oshithaB <ictdilunena@gmail.com>
2025-06-28 10:30:17 +0530

    db

--- database/data.sql
+++ database/data.sql
@@ -1,9 +1,8 @@
-ALTER TABLE quotation_items
-ADD COLUMN coil_inches DECIMAL(10,2) AFTER newsaleprice,
-ADD COLUMN peaces INT AFTER coil_inches;
-
-ALTER TABLE quotation_items
-ADD COLUMN newsaleprice DECIMAL(10,2) AFTER price;
-
-ALTER TABLE supplier_quotation
-ADD COLUMN status ENUM('requested', 'request_confirmed', 'receved') NOT NULL DEFAULT 'requested';
+CREATE TABLE expenses (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    created_by INT NOT NULL,
+    description TEXT NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    bill_number VARCHAR(50), -- Nullable by default
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
